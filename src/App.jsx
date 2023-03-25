4a5741ba48c0b916ad18dddc98792d51eab09d2e
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-25 14:56:46 +0530

    Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Routes from './components/Routes';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={darkTheme ? ' dark' : ''}>
+     <div className = "bg-gray-100 dark : bg-gray-900 dark : text-200 min-h-screen">
+      App
+     </div>
+     <h1 className='text-red-300'>Hello</h1>
     </div>
+    
   )
 }
 
